b09b025de26155eabb0e0d493562083c7c70639a
Alejololer <acarlos29112003@gmail.com>
2024-08-03 22:03:08 -0500

    Fue mi :eggplant:
    De hecho, fue el hecho de que la API está 100% funcional, pero ahora es case insensitive en circuito y dia, el dia también se deberá ingresar en español (hay un diccionario mágico dentro del .py :eggplant: )

--- Controllers/ModelController.cs
+++ Controllers/ModelController.cs
@@ -1,6 +1,5 @@
-using APIProyecto.Modelo;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace APIProyecto.Controllers
 {
@@ -11,14 +10,39 @@ namespace APIProyecto.Controllers
 
 
         [HttpGet()]
-        public async Task<IActionResult> GetModelo([FromQuery] Entrada entrada)
+        public async Task<IActionResult> GetModelo(int Hora, string Circuito, string Tipo, string DiaSemana, int Mes)
         {
+            Circuito = Circuito.ToUpper();
             // Valida el modelo
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            return Ok(new { Message = "Parametros enviados correctamente", entrada });
+            // Construimos el string de argumentos
+            string args = $"{Hora} \"{Circuito}\" \"{Tipo}\" {DiaSemana} {Mes}";
+            string pythonPath = "python";
+            string scriptPath = "./Resources/modelo.py";
+            var start = new ProcessStartInfo
+            {
+                FileName = pythonPath,
+                Arguments = $"{scriptPath} {args}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            string result;
+            using (var process = Process.Start(start))
+            {
+                result = await process.StandardOutput.ReadToEndAsync();
+                string error = await process.StandardError.ReadToEndAsync();
+                if (!string.IsNullOrEmpty(error))
+                {
+                    return StatusCode(500, error);
+                }
+            }
+            return Ok(result);
         }
     }
 }
---
+++ Resources/modelo.py
@@ -0,0 +1,67 @@
+import joblib
+import sys
+import os
+import warnings
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+# Suprimir advertencias
+warnings.filterwarnings('ignore')
+#Direcciones de archivos
+script_dir = os.path.dirname(os.path.abspath(__file__))
+kmeans_model_path = os.path.join(script_dir, 'kmeans_model.pkl')
+label_encoders_path = os.path.join(script_dir, 'label_encoders.pkl')
+# Cargar el modelo KMeans y los LabelEncoders
+kmeans_loaded = joblib.load(kmeans_model_path)
+label_encoders = joblib.load(label_encoders_path)
+
+
+# Obtener los parámetros de la línea de comandos
+if len(sys.argv) != 6:
+    print("Uso: python modelo.py <Hora> <Circuito> <Tipo> <Día de la Semana> <month>")
+    sys.exit(1)
+
+hora = int(sys.argv[1])
+circuito = sys.argv[2]
+tipo = sys.argv[3]
+dia_de_la_semana = sys.argv[4]
+month = int(sys.argv[5])
+#Diccionario para permitir entrada en español
+dia_semana_dict = {
+    'lunes': 'Monday',
+    'martes': 'Tuesday',
+    'miércoles': 'Wednesday',
+    'jueves': 'Thursday',
+    'viernes': 'Friday',
+    'sábado': 'Saturday',
+    'domingo': 'Sunday'
+}
+#Todo a minúsculas
+dia_de_la_semana = dia_de_la_semana.lower()
+#Usamos el diccionario
+dia_de_la_semana = dia_semana_dict.get(dia_de_la_semana, 'Desconocido')
+entrada = [hora, circuito, tipo, dia_de_la_semana, month]
+
+# Supongamos que 'nuevos_datos' es un DataFrame con los datos que quieres predecir
+columnas = ['Hora', 'Circuito', 'Tipo', 'Día de la Semana', 'month']
+nuevos_datos = pd.DataFrame([entrada], columns=columnas)
+
+# Aplicar la transformación a las nuevas columnas usando los LabelEncoders
+for column in nuevos_datos.columns:
+    le = label_encoders[column]
+    # Aplicar la transformación a la nueva columna
+    nuevos_datos[column] = le.transform(nuevos_datos[column])
+
+# Predecir usando el modelo cargado
+predicciones = kmeans_loaded.predict(nuevos_datos)
+prediccion = predicciones[0]
+
+# Clasificar la predicción
+if prediccion == 0:
+    clasificacion = "Llamada de importancia grave"
+elif prediccion == 1:
+    clasificacion = "Llamada de importancia moderada"
+elif prediccion == 2:
+    clasificacion = "Ruidos e incidentes de menor importancia"
+
+print(f"La llamada fue clasificada en: {clasificacion}")
